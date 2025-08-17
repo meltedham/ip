8e62e2649a82b50356199bc67a62231928b1b467
meltedham <yeowraeyen@gmail.com>
2025-08-18 00:52:57 +0800

    Level-0

---
+++ src/main/java/Meat.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Meat {
+    public static void main(String[] args) {
+        String name = "Meat";
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
