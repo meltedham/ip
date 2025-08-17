ad522aae1bcdbad16ea063e300b1c23203d35200
meltedham <yeowraeyen@gmail.com>
2025-08-18 01:25:56 +0800

    Level-2

--- src/main/java/Meat.java
+++ src/main/java/Meat.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Meat {
@@ -6,12 +7,22 @@ public class Meat {
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?\n");
 
+        ArrayList<String> list = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
+        String input = scanner.nextLine();
         while (!input.equals("bye"))
         {
-            System.out.println(input);
-            input = scanner.next();
+            if (input.equals("list"))
+            {
+                for (int i = 0; i < list.size(); i++)
+                {
+                    System.out.println(i + 1 + ". " + list.get(i));
+                }
+            } else {
+                list.add(input);
+                System.out.println("added: " + input);
+            }
+            input = scanner.nextLine();
         }
 
         System.out.println("Bye. Hope to see you again soon!");
