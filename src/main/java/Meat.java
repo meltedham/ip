f054cb2d7b6f72d6e01fb31ba310c1ff4027a491
meltedham <yeowraeyen@gmail.com>
2025-08-18 00:54:29 +0800

    Level-1

--- src/main/java/Meat.java
+++ src/main/java/Meat.java
@@ -5,6 +5,15 @@ public class Meat {
         String name = "Meat";
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?\n");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        while (!input.equals("bye"))
+        {
+            System.out.println(input);
+            input = scanner.next();
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
